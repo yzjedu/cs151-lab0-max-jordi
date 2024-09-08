a15f85ff951dfae5aa62a50b96b458a002a8559b
mxrice <mxrice@loyola.edu>
2024-09-08 16:17:37 -0400

    Printed another six facts about Jordi

--- Lab0.py
+++ Lab0.py
@@ -1,6 +1,21 @@
+# Programmers: Max and Jordi
+# Course: CS151 Dr.Yalew
+# Due date: 9/12/24
+# Lab Assignment: 0
+# Problem Statement: Output information about the programmers
+# Data In: There is no user input in this program
+# Data Out: Names of programmers and five facts about them
+# Credits: Max and Jordi
 print("CS151 Lab 0")
 print("My partners name is Max")
 print("He is from Wilmette,Illinois")
 print("Max has three siblings and is the youngest")
 print("He has two dogs and a fish he takes care of")
 print("Max is on the Loyola Swimming Team")
+print("my partner name is Jordi")
+print("He is from long island New York")
+print("He has two dogs")
+print("He is a part of the loyola club soccer team")
+print("He has an older brother")
+print("He goes to Ecuador")
+print("his favorite color is green")
